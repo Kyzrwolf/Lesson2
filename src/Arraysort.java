95db9a5476a3be7a9c002e9d6d912a348106ab9c
Kyzrwolf <kaizerzavr@gmail.com>
2023-04-02 19:34:52 +0600

    Исправил замечания, добавил проверки

--- src/Arraysort.java
+++ src/Arraysort.java
@@ -1,35 +1,44 @@
-
 public class Arraysort {
 
     public static void main(String[] args) {
-        int[] Array = {1, 5, 11, 4, 3, 8, 7, 6, 12, 2, 0};
+        int[] array = new int[]{1, 5, 11, 4, 3, 10, 8, 7, 6, 12, 2, 0};
+
+        printArray(array);
+        sortArray(array, true);
+        printArray(array);
+        sortArray(array, false);
+        printArray(array);
+    }
 
 
-        /** Пузырьковая сортировка. Сравнивается предыдущий и следующий элемент массива.
-         * для сортировки в обратную сторону изменить в строке 16 ">" на "<"
-         */
+    public static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(arr[i]);
+            if (i == arr.length - 1) {
+                System.out.print("]");
+            }
+        }
+        System.out.println("");
+    }
+
+    public static void sortArray(int[] array, boolean ascending) {
+        int n = array.length;
         boolean swapped = true;
-        int n = Array.length;
         while (swapped) {
             swapped = false;
             for (int i = 1; i < n; i++) {
-                if (Array[i - 1] > Array[i]) {
-                    int temp = Array[i - 1];
-                    Array[i - 1] = Array[i];
-                    Array[i] = temp;
+                if ((ascending && array[i] < array[i - 1]) || (!ascending && array[i] > array[i - 1])) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
                     swapped = true;
                 }
             }
-            n--;
         }
-        for (int i = 0; i < Array.length; i++) {
-            System.out.print(Array[i]);
-            if (i != Array.length - 1) {
-                System.out.print(", "); // вывод элементов массива через запятую (на конце - без запятой :)
-            }
-        }
-
     }
 }
-
 
--- src/Euclid.java
+++ src/Euclid.java
@@ -1,30 +1,39 @@
-import java.util.Scanner;
 
 public class Euclid {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите два целых числа: ");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        sc.close();
-        System.out.println("Наибольший общий делитель: " + getGCD(a, b));
-        System.out.println("Наименьшее общее кратное: " + getLCM(a, b));
+
+        System.out.println(getGCD(25,45) == 5);
+        System.out.println(getGCD(45,25) == 5);
+        System.out.println(getGCD(17,23) == 1);
+        System.out.println(getGCD(Integer.MAX_VALUE,Integer.MAX_VALUE) == Integer.MAX_VALUE);
+        System.out.println(getLCM(25, 45) == 225);
+        System.out.println(getLCM(45, 25) == 225);
+        System.out.println(getLCM(7,8) == 56);
+        System.out.println(getLCM(Integer.MAX_VALUE, Integer.MAX_VALUE) == Integer.MAX_VALUE);
 
     }
 
     private static int getGCD(int a, int b) {
-        while (a != b) {
-            if (a > b) {
-                a = a - b;
-            } else {
-                b = b - a;
+        while (a > 0 && b > 0) {
+            if ((b % a) == 0) {
+                return a;
             }
+            b %= a;
+            if ((a % b) == 0) {
+                return b;
+            }
+            a %= b;
+        }
+        if (a == 0) {
+            return b;
         }
         return a;
     }
+
     private static int getLCM(int a, int b) {
-        return a*b / getGCD(a,b);
+        long lcm = ((long) a / getGCD(a, b)) * b;
+        return (int) lcm;
     }
 
 }
